9521422c89d3d4ec974de7482ec226b3d292c39e
Bartlomiej Korytkowski <korytkowskibartlomiej@gmail.com>
2020-09-26 11:55:59 +0200

    add Boarder

---
+++ src/components/Board/Board.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Baord extends Component {
+    render() {
+        return (
+            <div className="board">
+                <div>Hello!</div>
+            </div>
+        )
+    }
+}
+
+export default Baord;
